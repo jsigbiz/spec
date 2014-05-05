72237771a85bad8540fbe69d7b28849196733858
Raynos <raynos2@gmail.com>
2014-05-04 19:43:46 -0700

    no pre whitespace

--- parser/program.js
+++ parser/program.js
@@ -2,8 +2,7 @@ var Parsimmon = require('parsimmon');
 
 var statement = require('./statement.js');
 
-var line = Parsimmon.optWhitespace
-    .then(statement)
+var line = statement
     .skip(Parsimmon.optWhitespace);
 
 var program = line.many().map(function (statements) {
