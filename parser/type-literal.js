995f83fc45be2c39d71240a82949ea26e525f185
Raynos <raynos2@gmail.com>
2014-05-04 22:57:53 -0700

    add generic types

--- parser/type-literal.js
+++ parser/type-literal.js
@@ -1,5 +1,7 @@
 var Parsimmon = require('parsimmon');
 
+var join = require('./lib/join.js');
+
 var builtinTypes = [
     'String', 'Number', 'Object', 'void', 'Any', 'Array'
 ];
@@ -30,4 +32,35 @@ var typeLiteral = Parsimmon.alt(
     customType
 )
 
-module.exports = typeLiteral
+var genericArgs = Parsimmon.lazy(function () {
+    return join(
+        typeDefinition,
+        Parsimmon.optWhitespace
+            .then(Parsimmon.string(','))
+            .then(Parsimmon.optWhitespace)
+    );
+})
+
+var genericExpression = Parsimmon.string('<')
+    .then(genericArgs)
+    .skip(Parsimmon.string('>'))
+
+var genericLiteral = Parsimmon.seq(
+    typeLiteral,
+    genericExpression.times(0, 1)
+).map(function (list) {
+    if (list[1].length === 0) {
+        return list[0]
+    }
+
+    return {
+        type: 'genericLiteral',
+        value: list[0],
+        generics: list[1][0]
+    }
+})
+
+
+module.exports = genericLiteral
+
+var typeDefinition = require('./type-definition.js');
--- test/generics.js
+++ test/generics.js
@@ -2,11 +2,32 @@ var test = require('tape');
 
 var parse = require('../parser.js');
 
-test.skip('foo := Object<String, Any>', function (assert) {
+test('foo := Object<String, Any>', function (assert) {
     var content = 'foo := Object<String, Any>'
     var result = parse(content).statements[0]
 
-    console.log('result', result)
+    assert.equal(result.type, 'assignment')
+    assert.equal(result.identifier, 'foo')
+    assert.deepEqual(result.typeExpression, {
+        type: 'genericLiteral',
+        value: {
+            type: 'typeLiteral',
+            builtin: true,
+            name: 'Object'
+        },
+        generics: [{
+            type: 'typeLiteral',
+            builtin: true,
+            name: 'String',
+            label: null
+        }, {
+            type: 'typeLiteral',
+            builtin: true,
+            name: 'Any',
+            label: null
+        }],
+        label: null
+    })
 
     assert.end()
 });
