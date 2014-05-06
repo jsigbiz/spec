93e71ddafab238d2e9dd5320cc0c3b3853a515b3
Raynos <raynos2@gmail.com>
2014-05-06 00:33:33 -0700

    _ is a valid thing in a prop name

--- parser/type-object.js
+++ parser/type-object.js
@@ -3,7 +3,7 @@ var Parsimmon = require('parsimmon');
 var AST = require('../ast.js');
 var join = require('./lib/join.js');
 
-var objectKey = Parsimmon.regex(/[a-z\?]+/i)
+var objectKey = Parsimmon.regex(/[_a-z\?]+/i)
     .skip(Parsimmon.string(':'))
     .skip(Parsimmon.optWhitespace)
 
