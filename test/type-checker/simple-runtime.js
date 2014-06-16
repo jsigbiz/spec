636190709702bd993bf67951bcb0d5fdcc1a03a8
Raynos <raynos2@gmail.com>
2014-06-16 01:11:10 -0700

    more tests

--- test/type-checker/simple-runtime.js
+++ test/type-checker/simple-runtime.js
@@ -3,7 +3,7 @@ var path = require('path')
 
 var compile = require('../../bin/type-check.js')
 
-test('compile simple-runtime', function (assert) {
+test('compile good example 1', function (assert) {
     var file = path.join(__dirname, '..',
         'fixtures', 'simple-runtime', 'good-example-1.js')
 
@@ -14,10 +14,38 @@ test('compile simple-runtime', function (assert) {
 
         assert.ok(meta)
 
-        console.log('meta', meta)
-
         assert.ok(meta && meta.identifiers.require)
         assert.ok(meta && meta.identifiers.sum)
+
+        assert.end()
+    })
+})
+
+test('compile bad example 1', function (assert) {
+    var file = path.join(__dirname, '..',
+        'fixtures', 'simple-runtime', 'bad-example-1.js')
+
+    compile({
+        _: [file]
+    }, function (err, meta) {
+        assert.ok(err)
+
+        assert.equal(err.message, 'expected string got Number')
+
+        assert.end()
+    })
+})
+
+test('compile bad example 1', function (assert) {
+    var file = path.join(__dirname, '..',
+        'fixtures', 'simple-runtime', 'bad-example-2.js')
+
+    compile({
+        _: [file]
+    }, function (err, meta) {
+        assert.ok(err)
+
+        assert.equal(err.message, 'expected string got Number')
 
         assert.end()
     })
