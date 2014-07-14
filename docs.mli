903136398a02cce2bdf7ad8bab5acf3899f462f5
Raynos <raynos2@gmail.com>
2014-07-13 21:30:33 -0700

    refactor to latests standard

--- docs.mli
+++ docs.mli
@@ -1,107 +1,107 @@
-type GenericE := {
+type GenericE : {
     type: "genericLiteral",
     value: TypeExpression,
     generics: Array<TypeExpression>,
-    label: String || null,
+    label: String | null,
     optional: Boolean
 }
 
-type FunctionE := {
+type FunctionE : {
     type: "function",
     args: Array<TypeExpression>,
     result: TypeExpression,
-    thisArg: TypeExpression || null,
-    label: String || null,
+    thisArg: TypeExpression | null,
+    label: String | null,
     optional: Boolean
 }
 
-type ValueE := {
+type ValueE : {
     type: "valueLiteral",
     value: String,
     name: String,
-    label: String || null,
+    label: String | null,
     optional: Boolean
 }
 
-type LiteralE := {
+type LiteralE : {
     type: "typeLiteral",
     name: String,
     builtin: Boolean,
-    label: String || null,
+    label: String | null,
     optional: Boolean
 }
 
-type UnionE := {
+type UnionE : {
     type: "unionType",
     unions: Array<TypeExpression>,
-    label: String || null,
+    label: String | null,
     optional: Boolean
 }
 
-type IntersectionE := {
+type IntersectionE : {
     type: "intersectionType",
     intersections: Array<TypeExpression>,
-    label: String || null,
+    label: String | null,
     optional: Boolean
 }
 
-type KeyValue := {
+type KeyValue : {
     type: "keyValue",
     key: String,
     value: TypeExpression,
     optional: Boolean
 }
 
-type ObjectE := {
+type ObjectE : {
     type: "object",
     keyValues: Array<KeyValue>,
-    label: String || null,
+    label: String | null,
     optional: Boolean
 }
 
-type TupleE := {
+type TupleE : {
     type: "tuple",
     values: Array<TypeExpression>,
-    label: String || null,
+    label: String | null,
     optional: Boolean
 }
 
-type TypeExpression := ObjectE || UnionE || LiteralE || FunctionE ||
-    ValueE || GenericE || TupleE || IntersectionE
+type TypeExpression : ObjectE | UnionE | LiteralE | FunctionE |
+    ValueE | GenericE | TupleE | IntersectionE
 
-type Assignment := {
+type Assignment : {
     type: "assignment",
     identifier: String,
     typeExpression: TypeExpression
 }
 
-type TypeDeclaration := {
+type TypeDeclaration : {
     type: "typeDeclaration",
     identifier: String,
     typeExpression: TypeExpression,
     generics: Array<LiteralE>
 }
 
-type Import := {
+type Import : {
     type: "import",
     dependency: String,
     types: Array<LiteralE>
 }
 
-type Statement := Import || TypeDeclaration || Assignment
+type Statement : Import | TypeDeclaration | Assignment
 
-type Program := {
+type Program : {
     type: "program",
     statements: Array<Statement>
 }
 
-type AST := {
+type AST : {
     program: (Array<Statement>) => Program,
     typeDeclaration: (String, TypeExpression) => TypeDeclaration,
     assignment: (String, TypeExpression) => Assignment,
     importStatement: (String, Array<LiteralE>) => Import,
     object: (
-        keyValues: Array<KeyValue> || Object<String, TypeExpression>,
+        keyValues: Array<KeyValue> | Object<String, TypeExpression>,
         label?: String
     ) => ObjectE,
     union: (Array<TypeExpression>, label?: String, opts?: {
@@ -132,6 +132,6 @@ type AST := {
     }) => TupleE
 }
 
-jsig/ast := AST
+jsig/ast : AST
 
-jsig/parser := (content: String) => Program
+jsig/parser : (content: String) => Program
